ed662ce39787b45e0dec23802250f437d484797d
ack81 <akinney031081@gmail.com>
2015-07-18 03:33:49 -0400

    silent install

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 ## check for install zsh
+echo -n "Installing Packages... "
 sudo yum -d0 -e0 -y install \
 zsh \
 vim-enhanced \
@@ -15,60 +16,76 @@ gcc \
 patch \
 perl \
 python-devel \
-python
+python \
+> /dev/null
+echo "Done"
 
 ## pip packages
-sudo pip install --upgrade powerline-status
-sudo pip install --upgrade pylint-common
-sudo pip install --upgrade pylint
-sudo pip install --upgrade python-ldap
+echo -n "Installing Python modules... "
+sudo pip install --upgrade --quiet powerline-status
+sudo pip install --upgrade --quiet pylint-common
+sudo pip install --upgrade --quiet pylint
+sudo pip install --upgrade --quiet python-ldap
+echo "Done"
 
 ## ruby gems
-sudo gem install puppet-lint
-sudo gem install ruby-lint
-sudo gem install bond
-sudo gem install json --platform=ruby
-sudo gem install parser
-sudo gem install ruby-beautify
-sudo gem install rgen
-sudo gem install rdoc-data
-sudo gem install rdoc
-sudo gem install pry
-sudo gem install r10k
+echo -n "Installing Ruby gems "
+gem list puppet-lint | grep -sq "^puppet-lint " || sudo gem install puppet-lint 1> /dev/null
+gem list ruby-lint | grep -sq "^ruby-lint " || sudo gem install ruby-lint 1> /dev/null
+gem list bond | grep -sq "^bond " || sudo gem install bond 1> /dev/null
+gem list json | grep -sq "^json " || sudo gem install json --platform=ruby 1> /dev/null
+gem list parser | grep -sq "^parser " || sudo gem install parser 1> /dev/null
+gem list ruby-beautify | grep -sq "^ruby-beautify " || sudo gem install ruby-beautify 1> /dev/null
+gem list rgen | grep -sq "^rgen " || sudo gem install rgen 1> /dev/null
+gem list rdoc-data | grep -sq "^rdoc-data " || sudo gem install rdoc-data 1> /dev/null
+gem list rdoc | grep -sq "^rdoc " || sudo gem install rdoc 1> /dev/null
+gem list pry | grep -sq "^pry " || sudo gem install pry 1> /dev/null
+gem list r10k | grep -sq "^r10k " || sudo gem install r10k 1> /dev/null
+echo "Done"
 
 ## Install powerline fonts
+echo -n "Installing powerline fonts... "
 if [ ! -d /usr/local/share/fonts/powerline ]; then
-  sudo git clone https://github.com/powerline/fonts.git /usr/local/share/fonts/powerline
+  sudo git clone -q https://github.com/powerline/fonts.git /usr/local/share/fonts/powerline
   sudo wget -qO /usr/local/share/fonts/powerline/PowerlineSymbols.otf \
     https://raw.githubusercontent.com/powerline/powerline/develop/font/PowerlineSymbols.otf
-  sudo wget -qO /etc/fonts/conf.d/font/10-powerline-symbols.conf \
+  sudo wget -qO /etc/fonts/conf.d/10-powerline-symbols.conf \
     https://raw.githubusercontent.com/powerline/powerline/develop/font/10-powerline-symbols.conf
+else
+  cd /usr/local/share/fonts/powerline
+  sudo git pull -q
 fi
-sudo fc-cache -vf
+sudo fc-cache -f
+echo "Done"
 
 ## check for /opt/vim-ide and clone with submodules
+echo -n "Installing VIM-IDE... "
 if [ ! -d /opt/vim-ide ];then
   sudo git clone https://github.com/ack81/vim-ide.git /opt/vim-ide
   cd /opt/vim-ide
-  sudo git submodule update --init
+  sudo git submodule --quiet update --init
 else
   cd /opt/vim-ide
-  sudo git pull
+  sudo git pull -q
 fi
-
-sudo cp -Rvfu /opt/vim-ide/vim /etc/
-sudo cp -vfu /opt/vim-ide/skel/.irbrc /etc/skel/
-sudo cp -vfu /opt/vim-ide/skel/.pythonrc /etc/skel/
-sudo cp -vfu /opt/vim-ide/skel/.vimrc /etc/skel/
-sudo cp -vfu /opt/vim-ide/profile.d/* /etc/profile.d/
+sudo cp -Rfu /opt/vim-ide/vim /etc/
+sudo cp -fu /opt/vim-ide/skel/.irbrc /etc/skel/
+sudo cp -fu /opt/vim-ide/skel/.pythonrc /etc/skel/
+sudo cp -fu /opt/vim-ide/skel/.vimrc /etc/skel/
+sudo cp -fu /opt/vim-ide/profile.d/* /etc/profile.d/
+echo "Done"
 
 ## clone and install oh-my-zsh
-if [ ! -d ~/.oh-my-zsh ];then
-  git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-  [ -e ~/.zshrc ] || cp -vf ~/.zshrc ~/.zshrc.orig
-  cp -vf ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
-  sudo chsh -s /bin/zsh ${LOGNAME}
+echo -n "Installing oh-my-zsh... "
+if [ ! -d /opt/oh-my-zsh ];then
+  sudo git clone -q git://github.com/robbyrussell/oh-my-zsh.git /opt/oh-my-zsh
 else
-  cd ~/.oh-my-zsh
-  git pull
+  cd /opt/oh-my-zsh
+  sudo git pull -q
 fi
+if [ ! -e ~/.oh-my-zsh ];then
+  cp -f ~/.zshrc ~/.zshrc.orig
+  cp -f /opt/oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+  sudo chsh -s /bin/zsh ${LOGNAME}
+fi
+echo "Done"

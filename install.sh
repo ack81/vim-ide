1a9c5223c069873acbddbbec5bc19b8eb8638926
ack81 <akinney031081@gmail.com>
2015-07-17 22:43:28 -0400

    install comments

--- install.sh
+++ install.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
+## check for install zsh
 rpm -qa zsh | grep "^zsh-" && yum -d0 -e0 -y install zsh
 
-if [ ! -d vim-ide ];then
+## check for /opt/vim-ide and clone with submodules
+if [ ! -d /opt/vim-ide ];then
   git clone https://github.com/ack81/vim-ide.git /opt/vim-ide
   cd /opt/vim-ide
   git submodule update --init
-  [ ! -d /etc/vim ] && cp -R vim /etc/
-  cp skel/.* /etc/skel
-  cp profile.d/* /etc/profile.d/
+  [ ! -d /etc/vim ] && cp -i-vfR vim /etc/
+  cp -vf skel/.* /etc/skel
+  cp -vf profile.d/* /etc/profile.d/
 fi
+
+## clone and install oh-my-zsh
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 cp ~/.zshrc ~/.zshrc.orig
 cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
